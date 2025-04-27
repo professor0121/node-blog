a5a9ffe98753f3fb23b3aa070fcdee20b2e1bf3d
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-04-27 08:46:34 +0530

    blog-server is done

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const http=require('http');
 const port=process.env.PORT || 5000;
 const router=require('./routes/router.js');
+// const email=require('./nodemailer/email.js');
+
 
 http.createServer((req,res)=>{
     router(req,res);
+    res.writeHead(200,{'Content-Type':'text/html'});
+    // email();
 }).listen(port,()=>{
     console.log(`Server running at http://localhost:${port}/`);
 })
+
+// const http=require('http');
+
+// http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-Type':'text/html'});
+//     res.end('<h1>Server is running</h1>');
+// }).listen(5000,()=>{
+//     console.log('Server running at http://localhost:5000/');
+// }   )
